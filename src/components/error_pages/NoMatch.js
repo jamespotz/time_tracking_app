20dbac21882821db11316a2abc7a8f78258c7940
James Robert Rooke <thepoltergeist23@gmail.com>
2018-10-04 10:22:53 +0800

    update 404 page

--- src/components/error_pages/NoMatch.js
+++ src/components/error_pages/NoMatch.js
@@ -6,7 +6,7 @@ const NoMatch = ({ location, history }) => (
     <div className="text-center">
       <h1 className="text-grey-dark" style={{fontSize: '16em'}}>4<div className="bigO"></div>4</h1>
       <h3 className="text-grey-light">
-        No match for <code>{location.pathname}</code>
+        Sorry, Page Not Found
       </h3>
       <button className="bg-grey text-white hover:bg-grey-dark px-3 py-2 mt-3 rounded" onClick={history.goBack}>Go Back</button>
     </div>
