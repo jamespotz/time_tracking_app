fa4c24dee4cd96fe5d8e4db1ad860d51a4fd8d23
James Robert Rooke <thepoltergeist23@gmail.com>
2018-10-04 16:36:18 +0800

    add 404 image

--- src/components/error_pages/NoMatch.js
+++ src/components/error_pages/NoMatch.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './no-match-style.css'
+import OhSnap from './oh-snap.svg'
 
 const NoMatch = ({ location, history }) => (
   <div className="flex flex-col justify-center items-center bg-white h-screen">
     <div className="text-center">
-      <h1 className="text-grey-dark" style={{fontSize: '16em'}}>4<div className="bigO"></div>4</h1>
+      
+      <h1 className="text-grey-dark" style={{fontSize: '16em'}}>4<img src={OhSnap} alt="oh snap" className="mx-3" height="180" width="180" />4</h1>
       <h3 className="text-grey-light">
         Sorry, Page Not Found
       </h3>
