a6c909fb98a59aebe597929c124524529b501f35
James Robert Rooke <thepoltergeist23@gmail.com>
2018-10-05 11:52:57 +0800

    add commit hitting a moon in nomatch page

--- src/components/error_pages/NoMatch.js
+++ src/components/error_pages/NoMatch.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './no-match-style.css'
-import OhSnap from './oh-snap.svg'
+import { ReactComponent as OhSnap } from './oh-no.svg'
 
 const NoMatch = ({ location, history }) => (
   <div className="flex flex-col justify-center items-center bg-white h-screen">
     <div className="text-center">
       
-      <h1 className="text-grey-dark" style={{fontSize: '16em'}}>4<img src={OhSnap} alt="oh snap" className="mx-3" height="180" width="180" />4</h1>
+      <h1 className="text-grey-dark" style={{fontSize: '16em'}}>4<OhSnap   width="200" height="200" className="mx-4"/>4</h1>
       <h3 className="text-grey-light">
         Sorry, Page Not Found
       </h3>
